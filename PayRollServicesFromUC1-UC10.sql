0e75e3c4a68a672277e567104d1c7dbf13f074cd
PrajaktaShewale <shewalep94@gmail.com>
2023-01-06 12:27:22 +0530

    [Prajakta]Add:Program to write in mssql for payroll services in that i created from uc1-10 in 1 file

--- PayRollServicesFromUC1-UC10.sql
+++ PayRollServicesFromUC1-UC10.sql
@@ -31,6 +31,25 @@ alter table employee_payroll add Empaddress varchar(200) not null default 'Pune'
 alter table employee_payroll add Department varchar(200) not null default 'IT'
 select*from employee_payroll;
 
+--UC9add Basic Pay,Deductions, Taxable Pay,Income Tax, Net Pay for that remove salary column
+Alter Table dbo.employee_payroll
+DROP COLUMN salary;
+Alter Table dbo.employee_payroll
+ADD BasicPay bigint NULL,Deductions bigint not null default 0.00,TaxablePay bigint not null default 0.00,IncomeTax bigint not null default 0.00,
+NetPay bigint not null default 0.00;
+update employee_payroll SET BasicPay=30000 where Name='raj'
+update employee_payroll SET BasicPay=45000 where Name='Bill'
+update employee_payroll SET BasicPay=50000 where Name='Ram'
+update employee_payroll SET BasicPay=90000 where Name='Rohan'
+update employee_payroll SET BasicPay=80000 where Name='Rma'
+update employee_payroll SET BasicPay=35000 where Name='Charlie'
+update employee_payroll SET BasicPay=56980 where Name='Radha'
+
+--UC10 Create 2 differnt id with same name but differnt department
+insert into employee_payroll values('Terissa','2018-5-2','F',6589321474,'Pune','Sales',25000,0.00,0.00,0.00,25000),
+('Terissa','2020-5-3','F',6989321474,'Pune','Marketing',35000,0.00,0.00,0.00,35000);
+
+
 --for delete complete table
 drop table employee_payroll;
 
